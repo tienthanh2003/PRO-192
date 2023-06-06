d03e051e4ff7da4927a2c7d21efb1b51ab921c8a
tienthanh2003 <133749583+tienthanh2003@users.noreply.github.com>
2023-06-06 22:57:16 +0700

    Add files via upload

---
+++ workshop/workshop4/ItemManager/src/validation/Validation.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validation;
+
+/**
+ *
+ * @author PC
+ */
+import java.util.Scanner;
+public class Validation {
+     private static final Scanner scanner = new Scanner(System.in);
+
+    public static int checkInputIntLimit(int min, int max) {
+        if (min > max) {
+            int temp = min;
+            min = max;
+            max = temp;
+        }
+
+        int value = Integer.MIN_VALUE;
+
+        while (true) {
+            try {
+                value = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.err.println("Please enter again");
+            }
+
+            if (value >= min && value <= max) {
+                break;
+            }
+        }
+
+        return value;
+    }
+    
+    public static boolean getBoolean() {
+        boolean value = false;
+        
+        while (true) {
+            try {
+                value = Boolean.parseBoolean(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("Please enter again");
+            }
+        }
+        
+        return value;
+    }
+}
